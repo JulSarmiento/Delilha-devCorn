8ea7522de8d1fc520f4b5fd7600c41f2a305d7f7
JulSarmiento <jasa1999@hotmail.com>
2023-11-16 17:48:02 -0500

    modelo de usuario terminado, pendiente de encriptacion de contraseñas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,74 @@
 import { Model, DataTypes } from 'sequelize';
 import Sequelize from '../utils/postgresql.config.js';
 
-class User extends Model {}
+// import { encryptedPassword } from '../utils/encryption.js';
+
+class User extends Model {
+
+  // static login(username, password) {
+  //   return this.findOne({
+  //     where: {
+  //       username,
+  //       password: encryptedPassword(password)
+  //     },
+  //   });
+  // }
+}
 
 User.init({
-
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+    unique: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+      isLowercase: true
+    }
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [9, 11],
+    },
+  },
+  active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'user',
+    validate: {
+      isIn: [['user', 'admin']],
+    }
+  }
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
 });
 
 export default User;
