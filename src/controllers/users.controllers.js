efaa48689cc948193c628270d5f75b1bd1de59ab
JulSarmiento <jasa1999@hotmail.com>
2023-12-02 19:13:48 -0500

    endpoints de listar y crear funcionando correctamente en postman

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,6 +1,7 @@
 import httpStatus from "http-status";
 import getToken from '../utils/atuh0.api.js'
 
+// Get all users
 export const getAllUsers = async (req, res, next) => {
   console.log('getAllUsers');
   try {
@@ -25,7 +26,7 @@ export const getAllUsers = async (req, res, next) => {
   };
 };
 
-
+// Get user by id
 export const getUserById = async (req, res, next) => {
   const { user_id } = req.params;
 
@@ -49,5 +50,53 @@ export const getUserById = async (req, res, next) => {
   } catch (error){
     next(error);
   };
-}
+};
 
+// Get user by email
+export const getUserByEmail = async (req, res, next) => {
+  try {
+    const token = await getToken();
+    const apiUrl = `${process.env.MANAGMENT_API_URL}api/v2/users-by-email?email=${req.params.email}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.access_token}`
+      },
+    };
+  
+    const response = await fetch(apiUrl, options);
+    res.status(httpStatus.OK).json({
+      succes: true,
+      data: await response.json(),
+    });
+  }
+  catch (error){
+    next(error);
+  };
+};
+
+// Create user
+export const createuser = async (req, res, next) => {
+  try {
+    const token = await getToken();
+    const apiUrl = `${process.env.MANAGMENT_API_URL}api/v2/users`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.access_token}`
+      },
+      body: JSON.stringify(req.body),
+    };
+
+    const response = await fetch(apiUrl, options);
+    res.status(httpStatus.OK).json({
+      succes: true,
+      data: await response.json(),
+    });
+  }
+  catch (error){
+    next(error);
+  };
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import httpStatus from 'http-status';
 import pkg from 'express-openid-connect';
-import { getAllUsers, getUserById } from '../controllers/index.js';
+import { getAllUsers, getUserById, getUserByEmail, createuser } from '../controllers/index.js';
 const router = express.Router();
 
 const {requiresAuth} = pkg;
 
+// auth0 validate user authenticated
 router.get('/', (req, res) => {
   if (req.oidc.isAuthenticated()) {
     res.status(httpStatus.OK).json({
@@ -18,16 +19,18 @@ router.get('/', (req, res) => {
       data: 'Unauthorized',
     });
   }
-
 });
 
+// auth0 profile loged user
 router.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
 
-
+// Crud for users with management api
 router.get('/listUsers', getAllUsers);
 router.get('/getUserById/:user_id', getUserById);
+router.get('/getUserByEmail/:email', getUserByEmail);
+router.post('/createUser', createuser);
 
 
 export default router;
